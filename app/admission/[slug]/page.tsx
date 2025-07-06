d2a4dfaa33fb3f853512ed02ee60f389ad12a3e6
Ayoub Ouaziz <aouaziz@c3r6p6.1337.ma>
2025-07-06 11:13:29 +0100

    fix deployment errors

--- app/admission/[slug]/page.tsx
+++ app/admission/[slug]/page.tsx
@@ -1,13 +1,13 @@
-import { programsData } from "@/app/data/programs"
 import ProgramDetailPageClient from "./ProgramDetailPageClient"
 
-export async function generateStaticParams() {
-  return programsData.map((program) => ({
-    slug: program.slug,
-  }))
+
+interface Props {
+  params: {
+    slug: string
+  }
 }
 
-export default function ProgramDetailPage({ params }: { params: { slug: string } }) {
+export default function ProgramDetailPage({ params }: Props) {
   const { slug } = params
   return <ProgramDetailPageClient slug={slug} />
 }
